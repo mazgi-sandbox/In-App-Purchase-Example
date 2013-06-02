94d41afa93e0a8efb26006177efc79d378fcf40b
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2013-06-02 17:23:51 +0900

    Create project.

---
+++ In-App-Purchase-Example/In-App-Purchase-Example/IAPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IAPAppDelegate.h
+//  In-App-Purchase-Example
+//
+//  Created by matsuki hidenori on 6/2/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IAPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ In-App-Purchase-Example/In-App-Purchase-Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  In-App-Purchase-Example
+//
+//  Created by matsuki hidenori on 6/2/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IAPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IAPAppDelegate class]));
+    }
+}
