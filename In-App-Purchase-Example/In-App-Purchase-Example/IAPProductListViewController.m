de449f3a633f5c074fd4a4b582cdc146d81b4b46
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2013-06-04 00:56:10 +0900

    Add transition to product detail view controller from product list view controller.

--- In-App-Purchase-Example/In-App-Purchase-Example/IAPProductListViewController.m
+++ In-App-Purchase-Example/In-App-Purchase-Example/IAPProductListViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "IAPProductListViewController.h"
+#import "IAPProductDetailViewController.h"
 
 @interface IAPProductListViewController ()
-
+@property (strong, nonatomic) NSMutableArray* productIdentifiers;
 @end
 
 @implementation IAPProductListViewController
@@ -18,15 +19,50 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        self.productIdentifiers = [NSMutableArray array];
     }
     return self;
 }
 
+#pragma mark - UITableViewDataSource methods
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.productIdentifiers count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"CellIdentifier"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"CellIdentifier"];
+    }
+    //TODO: replace product information
+    cell.textLabel.text = [self.productIdentifiers objectAtIndex:indexPath.row];
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate methods
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    IAPProductDetailViewController* productDetailViewController = [[IAPProductDetailViewController alloc]initWithNibName:nil bundle:nil];
+    //TODO: set product information
+    [self.navigationController pushViewController:productDetailViewController animated:YES];
+
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+#pragma mark - view lifecycle
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+
+    self.title = @"Products";
+    //TODO: temporarily hard-coding
+    [self.productIdentifiers addObject:@"consumable_01"];
+    [self.productIdentifiers addObject:@"consumable_02"];
 }
 
 - (void)didReceiveMemoryWarning
