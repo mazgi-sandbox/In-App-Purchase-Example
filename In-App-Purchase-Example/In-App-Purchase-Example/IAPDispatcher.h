ae65d5209342eea440a5f0b6b5e8c0de1b321daf
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2013-06-15 22:44:23 +0900

    Impl dispatcher methods.

--- In-App-Purchase-Example/In-App-Purchase-Example/IAPDispatcher.h
+++ In-App-Purchase-Example/In-App-Purchase-Example/IAPDispatcher.h
@@ -1,3 +1,8 @@
+/*!
+ @header
+ @abstract IAPDispatcher class header
+ */
+
 //
 //  IAPDispatcher.h
 //  In-App-Purchase-Example
@@ -10,37 +15,162 @@
 
 @class IAPDispatcher;
 
+/*!
+ @protocol
+ @abstract IAPデリゲート
+ @discussion IAPDispatcherからの通知を規定する.
+ @dependency IAPDispatcher
+ */
 @protocol IAPDispatcherDelegate <NSObject>
 
 @optional
 
-// load products
 /*!
+ @functiongroup Prepare
  @method
- @param dispatcher 
+ @abstract 事前処理終了通知(成功)
+ */
+- (void)dispatcher:(IAPDispatcher *)dispatcher didFinishPrepare:(NSArray *)productList;
+/*!
+ @functiongroup Prepare
+ @method
+ @abstract 事前処理終了通知(失敗)
+ */
+- (void)dispatcher:(IAPDispatcher *)dispatcher didFailPrepare:(NSArray *)productList withErrors:(NSArray *)errors;
+
+/*!
+ @functiongroup Load products
+ @method
+ @abstract 商品一覧取得開始前通知
+ @param dispatcher
  */
 - (void)dispatcherWillStartLoadingProductList:(IAPDispatcher *)dispatcher;
+/*!
+ @functiongroup Load products
+ @method
+ @abstract 商品一覧取得開始後通知
+ @param dispatcher
+ */
 - (void)dispatcherDidStartLoadingProductList:(IAPDispatcher *)dispatcher;
+/*!
+ @functiongroup Load products
+ @method
+ @abstract 商品一覧取得終了前通知
+ @param dispatcher
+ */
 - (void)dispatcherWillFinishLoadingProductList:(IAPDispatcher *)dispatcher;
+/*!
+ @functiongroup Load products
+ @method
+ @abstract 商品一覧取得終了通知(成功)
+ @param dispatcher
+ */
 - (void)dispatcher:(IAPDispatcher *)dispatcher didFinishLoadingProductList:(NSArray *)productList;
-- (void)dispatcher:(IAPDispatcher *)dispatcher didFailLoadingProductList:(NSArray *)productList withErrors:(NSError *)errors;
+/*!
+ @functiongroup Load products
+ @method
+ @abstract 商品一覧取得終了通知(失敗)
+ @param dispatcher
+ */
+- (void)dispatcher:(IAPDispatcher *)dispatcher didFailLoadingProductList:(NSArray *)productList withErrors:(NSArray *)errors;
 
-// purchase
+/*!
+ @functiongroup Purchase products
+ @method
+ @abstract 商品購入開始前通知
+ @param dispatcher
+ */
 - (void)dispatcherWillStartPurchase:(IAPDispatcher *)dispatcher;
-
-- (void)dispatcher:(IAPDispatcher *)dispatcher willStartPurchaseWithProductIdentifier:(NSString *)dispatcher;
-- (void)dispatcherDidStartLoadingProductList:(IAPDispatcher *)dispatcher;
-- (void)dispatcherWillFinishLoadingProductList:(IAPDispatcher *)dispatcher;
-- (void)dispatcher:(IAPDispatcher *)dispatcher didFinishLoadingProductList:(NSArray *)productList;
-- (void)dispatcher:(IAPDispatcher *)dispatcher didFailLoadingProductList:(NSArray *)productList withErrors:(NSError *)errors;
-
-// send receipt
+/*!
+ @functiongroup Purchase products
+ @method
+ @abstract 商品購入開始後通知
+ @param dispatcher
+ */
+- (void)dispatcherDidStartPurchase:(IAPDispatcher *)dispatcher;
+/*!
+ @functiongroup Purchase products
+ @method
+ @abstract 商品購入開始後通知
+ @param dispatcher
+ */
+- (void)dispatcher:(IAPDispatcher *)dispatcher willStartPurchaseWithProductIdentifier:(NSString *)productIdentifier;
+/*!
+ @functiongroup Purchase products
+ @method
+ @abstract 商品購入終了通知(成功)
+ @param dispatcher
+ */
+- (void)dispatcher:(IAPDispatcher *)dispatcher didFinishPurchaseProduct:(NSArray *)productList;
+/*!
+ @functiongroup Purchase products
+ @method
+ @abstract 商品購入終了通知(失敗)
+ @param dispatcher
+ */
+- (void)dispatcher:(IAPDispatcher *)dispatcher didFailPurchaseProduct:(NSArray *)productList withErrors:(NSArray *)errors;
 
 @end
 
+/*!
+ @class
+ @abstract IAP実行クラス
+ @discussion
+ <dl>
+ <dt>ライフサイクル／寿命</dt>
+ <dd>決済画面遷移時に生成され、決済後または他画面に遷移後破棄されることを想定する.<dd>
+ <dt>行うこと</dt>
+ <dd>与えられた商品IDによる決済</dd>
+ <dt>行わないこと</dt>
+ <dd>描画</dd>
+ <dd>StoreKitに含まれない通信</dd>
+ </dl>
+ */
 @interface IAPDispatcher : NSObject
+{
+    BOOL inProgress;
+}
+
+- (BOOL)addProductIdentifier:(NSString *)productIdentifier;
+
+/*!
+ @method
+ @abstract 前処理
+ @discussion
+ <dl>
+    <dt>行うこと</dt>
+    <dd>決済可能か検証する(iOS端末の設定で決済が許可されているか)</dd>
+    <dt>行わないこと</dt>
+    <dd>商品の取得</dd>
+    <dd>決済</dd>
+ </dl>
+ */
 - (void)prepare;
-- (void)loadProducts;
+/*!
+ @method
+ @abstract 商品一覧を取得する
+ @discussion
+ <dl>
+    <dt>行うこと</dt>
+    <dd>商品の取得</dd>
+    <dt>行わないこと</dt>
+    <dd>決済</dd>
+ </dl>
+ */
+- (void)loadProducts:(NSArray *)productList;
+/*!
+ @method
+ @abstract 決済を行う
+ @discussion
+ <dl>
+    <dt>行うこと</dt>
+    <dd>決済</dd>
+    <dt>行わないこと</dt>
+    <dd>商品の取得</dd>
+ </dl>
+ */
 - (void)purchaseProduct;
 @property (weak, nonatomic) id<IAPDispatcherDelegate> delegate;
+@property (readonly, nonatomic) NSArray *validProducts;
+@property (readonly, nonatomic) NSArray *invalidProducts;
 @end
--- In-App-Purchase-Example/In-App-Purchase-Example/IAPDispatcher.m
+++ In-App-Purchase-Example/In-App-Purchase-Example/IAPDispatcher.m
@@ -7,7 +7,55 @@
 //
 
 #import "IAPDispatcher.h"
+#import <StoreKit/StoreKit.h>
+
+enum IAPDispatcherErrorCode {
+    IAPDispatcherErrorCodeNoError = 0,
+    IAPDispatcherErrorCodeCanNotMakePayments,
+};
+
+static NSString * const kIAPDispatcherErrorDomain = @"IAPDispatcherError";
+static NSString * const kIAPDispatcherErrorUserInfoKeyReason = @"kIAPDispatcherStatusKeyPrepared";
+static NSString * const kIAPDispatcherStatusAndReasonKeyPrepared = @"kIAPDispatcherStatusKeyPrepared";
+@interface IAPDispatcher()
+@property (strong, nonatomic) NSMutableDictionary *statuses;
+@property (strong, nonatomic) NSMutableDictionary *reasons;
+@end
 
 @implementation IAPDispatcher
 
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        self.statuses = [NSMutableDictionary dictionary];
+        self.reasons = [NSMutableDictionary dictionary];
+    }
+    return self;
+}
+
+- (void)prepare
+{
+    // can make payments?
+    BOOL canMakePayments = [SKPaymentQueue canMakePayments];
+    // reset status and push new status.
+    [self.statuses setObject:[NSNumber numberWithBool:canMakePayments]
+                      forKey:kIAPDispatcherStatusAndReasonKeyPrepared];
+    if (!canMakePayments) {
+        NSDictionary *userInfo = [NSDictionary dictionaryWithObject:@"Can not make payments."
+                                                             forKey:kIAPDispatcherErrorUserInfoKeyReason];
+        NSError *err = [NSError errorWithDomain:kIAPDispatcherErrorDomain
+                                           code:IAPDispatcherErrorCodeCanNotMakePayments
+                                       userInfo:userInfo];
+        // reset reason and push error.
+        [self.reasons setObject:[NSMutableArray arrayWithObject:err]
+                         forKey:kIAPDispatcherStatusAndReasonKeyPrepared];
+        if ([self.delegate respondsToSelector:@selector(dispatcher:didFailPrepare:withErrors:)]) {
+            [self.delegate performSelector:@selector(dispatcher:didFailPrepare:withErrors:)
+                                withObject:self
+                                withObject:[self.reasons objectForKey:kIAPDispatcherStatusAndReasonKeyPrepared]];
+        }
+        return;
+    }
+}
 @end
